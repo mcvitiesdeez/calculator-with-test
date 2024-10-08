eb9c84d19ef97609fd5b4a67f1940b459c7c9a19
mcvitiesdeez <ott_94@hotmail.com>
2024-10-08 10:40:42 +0800

    Initialize front-end testing

--- src/tests/calculate.test.js
+++ src/tests/calculate.test.js
@@ -4,3 +4,23 @@ import { calculate } from "../calculate";
 test("calculate addition", ({ expect }) => {
     expect(calculate(5, 7, "+")).toBe(12);
 });
+
+test("calculate subtraction", ({ expect }) => {
+    expect(calculate(10, 5, "-")).toBe(5);
+});
+
+test("calculate multiplication", ({ expect }) => {
+    expect(calculate(5, 2, "*")).toBe(10);
+})
+
+test("calculate division", ({ expect }) => {
+    expect(calculate(10, 5, "/")).toBe(2);
+})
+
+test("calculate zero division", ({ expect }) => {
+    expect(calculate(10, 0, "/")).toBe("Error: Cannot divide by zero");
+})
+
+test("calculate invalid operator", ({ expect }) => {
+    expect(() => calculate(2, 3, "%")).toThrowError("Invalid Operator");
+})
